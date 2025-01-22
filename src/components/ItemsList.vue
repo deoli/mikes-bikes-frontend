55a8be1094cfd5424193ce99885e5015878b3a4a
deoli <deoli@hotmail.com>
2025-01-22 16:09:01 +0000

    ItemsList component

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <template v-for="item in resourceList" :key="item.id">
+      <div v-show="!item.parent_id" class="card col-sm m-3">
+        <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{ item.name }}</h5>
+          <p class="card-text">The item's description would go here if we had one.</p>
+          <router-link :to="'/' + resourceType + '/' + item.id" class="btn btn-primary">
+            Modify
+          </router-link>
+        </div>
+      </div>
+    </template>
+    <router-link :to="'/' + resourceType + '/new'" class="btn btn-primary">Add new {{ resourceType }}</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ItemsList',
+    props: ['resourceList', 'resourceType']
+  };
+</script>
--- src/views/BlueprintList.vue
+++ src/views/BlueprintList.vue
@@ -1,31 +1,25 @@
 <template>
   <div class="blueprint">
     <h1>These are the blueprints</h1>
-    <div class="row">
-      <template v-for="blueprint in $store.getters.stateBlueprints" :key="blueprint.id">
-        <div v-show="!blueprint.parent_id" class="card col-sm m-3">
-          <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">{{ blueprint.name }}</h5>
-            <p class="card-text">The item's description would go here if we had one.</p>
-            <router-link :to="'/blueprint/' + blueprint.id" class="btn btn-primary">
-              Modify
-            </router-link>
-          </div>
-        </div>
-      </template>
-      <router-link to="/blueprint/new" class="btn btn-primary">Add new blueprint</router-link>
-    </div>
+    <ItemsList
+      :resource-list="blueprintList"
+      :resource-type="'blueprint'"
+    />
   </div>
 </template>
 
 <script>
-  // import OptionsForm from '@/components/OptionsForm.vue';
+  import ItemsList from '@/components/ItemsList.vue';
   export default {
     name: 'BlueprintList',
-    // components: {
-    //   OptionsForm
-    // },
+    components: {
+      ItemsList
+    },
+    computed: {
+      blueprintList() {
+        return this.$store.getters.stateBlueprints;
+      }
+    },
     beforeCreate() {
       this.$store.getters.stateBlueprints || this.$store.dispatch('getBlueprints');
     }
