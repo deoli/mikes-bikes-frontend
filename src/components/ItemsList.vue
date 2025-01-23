0f47d0135664f734793b549b07ff4f0c3eb33b1d
deoli <deoli@hotmail.com>
2025-01-23 10:13:05 +0000

    edit Product

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -16,7 +16,9 @@
         <template v-if="resourceType === 'blueprint'">
           <ul class="list-group list-group-flush">
             <template v-for="product in productList" :key="product.id">
-              <li v-if="product.blueprint_id == item.id" class="list-group-item">{{ product.name }}</li>
+              <router-link v-if="product.blueprint_id == item.id" :to="'/product/' + product.id" class="list-group-item list-group-item-action">
+                  {{ product.name }}
+              </router-link>
             </template>
             <router-link :to="'/' + resourceType + '/' + item.id + '/product/new'" class="list-group-item list-group-item-action active">
               Add product
